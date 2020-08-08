4571f7c72c33676faba307a54f805f6a9e6430bc
prakash <prakashsaec@gmail.com>
2020-08-08 17:54:24 +0530

    minor changes

--- src/main/java/book/org/dxctraining/entities/Book.java
+++ src/main/java/book/org/dxctraining/entities/Book.java
@@ -7,12 +7,13 @@ public class Book {
 	private String name;
 	private double cost;
 	private Author author;
-	public Book(String name,double cost,Author author) {
-		this.name=name;
-		this.cost=cost;
-		this.author=author;
+
+	public Book(String name, double cost, Author author) {
+		this.name = name;
+		this.cost = cost;
+		this.author = author;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
@@ -24,40 +25,46 @@ public class Book {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Author getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(Author author) {
 		this.author = author;
 	}
+
 	public double getCost() {
 		return cost;
 	}
+
 	public void setCost(double cost) {
 		this.cost = cost;
 	}
+
 	@Override
 	public int hashCode() {
-		int hash=id.hashCode();
+		int hash = id.hashCode();
 		return hash;
 	}
-		@Override
-		public boolean equals(Object arg) {
-			if(this==arg) {
-				return true;
-			}
-			
-		   if(arg==null || !(arg instanceof Book) ) {
-				return false;
-		   }
-			
-		    Book that=(Book)arg;	
-			boolean isequal=this.id.equals(that.id);
-			return isequal;
+
+	@Override
+	public boolean equals(Object arg) {
+		if (this == arg) {
+			return true;
 		}
 
-	
+		if (arg == null || !(arg instanceof Book)) {
+			return false;
+		}
+
+		Book that = (Book) arg;
+		boolean isequal = this.id.equals(that.id);
+		return isequal;
+	}
+
 }
